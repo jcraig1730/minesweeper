757c775b495e4f24224775cc92f69fc619dee102
jared <jcraig1730@gmail.com>
2019-12-21 19:56:35 -0800

    add testing for in game functions; add reducers for UPDATE_BOARD and GAME_OVER events; add functions for handling left and right clicks on game board; update Cell component to call UPDATE_BOARD reducer on game click;

---
+++ tests/testGameboard.js
@@ -0,0 +1,860 @@
+// 10 x 10 grid with 30 mines
+module.exports = {
+  xCount: 10,
+  yCount: 10,
+  mineCount: 30,
+  board: [
+    [
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "00"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "01"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "02"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "03"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "04"
+      },
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "05"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "06"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "07"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "08"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "09"
+      }
+    ],
+    [
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "10"
+      },
+      {
+        isMine: true,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "11"
+      },
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "12"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "13"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "14"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "15"
+      },
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "16"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "17"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "18"
+      },
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "19"
+      }
+    ],
+    [
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "20"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "21"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "22"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "23"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "24"
+      },
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "25"
+      },
+      {
+        isMine: false,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "26"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "27"
+      },
+      {
+        isMine: false,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "28"
+      },
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "29"
+      }
+    ],
+    [
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "30"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "31"
+      },
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "32"
+      },
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "33"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "34"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "35"
+      },
+      {
+        isMine: true,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "36"
+      },
+      {
+        isMine: true,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "37"
+      },
+      {
+        isMine: false,
+        touching: 5,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "38"
+      },
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "39"
+      }
+    ],
+    [
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "40"
+      },
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "41"
+      },
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "42"
+      },
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "43"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "44"
+      },
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "45"
+      },
+      {
+        isMine: false,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "46"
+      },
+      {
+        isMine: true,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "47"
+      },
+      {
+        isMine: true,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "48"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "49"
+      }
+    ],
+    [
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "50"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "51"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "52"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "53"
+      },
+      {
+        isMine: false,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "54"
+      },
+      {
+        isMine: false,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "55"
+      },
+      {
+        isMine: false,
+        touching: 5,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "56"
+      },
+      {
+        isMine: false,
+        touching: 5,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "57"
+      },
+      {
+        isMine: true,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "58"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "59"
+      }
+    ],
+    [
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "60"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "61"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "62"
+      },
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "63"
+      },
+      {
+        isMine: true,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "64"
+      },
+      {
+        isMine: true,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "65"
+      },
+      {
+        isMine: true,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "66"
+      },
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "67"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "68"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "69"
+      }
+    ],
+    [
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "70"
+      },
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "71"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "72"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "73"
+      },
+      {
+        isMine: true,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "74"
+      },
+      {
+        isMine: true,
+        touching: 4,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "75"
+      },
+      {
+        isMine: false,
+        touching: 5,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "76"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "77"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "78"
+      },
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "79"
+      }
+    ],
+    [
+      {
+        isMine: true,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "80"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "81"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "82"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "83"
+      },
+      {
+        isMine: false,
+        touching: 3,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "84"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "85"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "86"
+      },
+      {
+        isMine: true,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "87"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "88"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "89"
+      }
+    ],
+    [
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "90"
+      },
+      {
+        isMine: true,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "91"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "92"
+      },
+      {
+        isMine: true,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "93"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "94"
+      },
+      {
+        isMine: false,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "95"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "96"
+      },
+      {
+        isMine: false,
+        touching: 1,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "97"
+      },
+      {
+        isMine: false,
+        touching: 2,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "98"
+      },
+      {
+        isMine: true,
+        touching: 0,
+        isFlagged: false,
+        isQuestioned: false,
+        isClicked: false,
+        id: "99"
+      }
+    ]
+  ],
+  minePositions: [
+    [4, 7],
+    [2, 9],
+    [1, 6],
+    [7, 4],
+    [6, 3],
+    [7, 5],
+    [6, 6],
+    [1, 1],
+    [0, 0],
+    [9, 9],
+    [1, 9],
+    [5, 8],
+    [9, 1],
+    [6, 5],
+    [3, 7],
+    [4, 8],
+    [9, 3],
+    [6, 4],
+    [6, 7],
+    [1, 2],
+    [8, 0],
+    [8, 7],
+    [7, 1],
+    [3, 9],
+    [6, 0],
+    [2, 0],
+    [4, 5],
+    [3, 6],
+    [2, 5],
+    [0, 5]
+  ]
+};
